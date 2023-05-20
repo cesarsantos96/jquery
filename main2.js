5dc32bd3558ec330b607e07a2eb755a9d82d0111
Cesar Santos <csar.santos18@gmail.com>
2023-05-20 09:49:21 -0500

    atividade do módulo 9

--- main2.js
+++ main2.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
-    $('button[type="sub"]').click(function () {
-        var novaTarefa = $("#nova-tarefa");
-        var novaTarefaLi = $("<li>");
-        var novaTarefaSpan = $("<span>").text(novaTarefa.val());
-        novaTarefaLi.append(novaTarefaSpan);
-        $("#tarefas").append(novaTarefaLi);
-        $("#nova-tarefa").val("");
-        novaTarefaSpan.click(function () {
-            $(this).toggleClass("done");
-        });
+    $('#submit-button').click(function () {
+        var novaTarefa = $("#nova-tarefa").val();
+        if (novaTarefa) {
+            var novaTarefaLi = $("<li></li>").text(novaTarefa);
+            $("#tarefas").append(novaTarefaLi);
+            $("#nova-tarefa").val("");
+            novaTarefaLi.click(function () {
+                $(this).toggleClass("done");
+            });
+        }
     });
 });
