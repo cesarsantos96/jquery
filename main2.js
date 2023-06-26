9adea0d74c597f4df92506de1d9c276f779a8e47
Cesar Santos <csar.santos18@gmail.com>
2023-06-26 10:07:50 -0500

    atividade do módulo 9

--- main2.js
+++ main2.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
         var novaTarefa = $("#nova-tarefa").val();
         if (novaTarefa) {
             var novaTarefaLi = $("<li></li>").text(novaTarefa);
-            $("#tarefas").append(novaTarefaLi);
+            $("#tarefas").appendTo(novaTarefaLi);
             $("#nova-tarefa").val("");
             novaTarefaLi.click(function () {
                 $(this).toggleClass("done");
